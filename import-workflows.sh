b57ec660955de81d1ed46e110dc29b96947f4255
MaraisL <louis.marais@inetum.com>
2025-04-24 10:47:43 +0200

    Fix workflow import at start

--- import-workflows.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-sudo docker compose exec -it pa-n8n n8n import:workflow --separate --input=/usr/src/app/workflows/
--- runtime/n8n/import-json.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-echo "Starting import ..."
-
-n8n import:workflow --separate --input=workflows/
-
-echo "Import DONE"
